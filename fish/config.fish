3bbdbdafe6d98b30ea019b072a8814b165f77f1e
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2014-08-25 21:08:10 +0100

    merge config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,7 @@ set fish_theme bobthefish
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
 #set fish_plugins autojump bundler git node rvm
-set fish_plugins rvm git bundler
+set fish_plugins rvm git bundler node rails
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
@@ -16,15 +16,25 @@ set fish_plugins rvm git bundler
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
+# Docker setup
 set -x DOCKER_HOST tcp://192.168.59.103:2375
+function docker-enter
+  boot2docker ssh '[ -f /var/lib/boot2docker/nsenter ] || docker run --rm -v /var/lib/boot2docker/:/target jpetazzo/nsenter'
+  boot2docker ssh -t sudo /var/lib/boot2docker/docker-enter $argv
+end
+
 
 # todo.txt related
 alias t="todo.sh"
 alias te="subl ~/Dropbox/todo/todo.txt"
 set -x TODOTXT_DEFAULT_ACTION ls
 
+# local path
+set PATH $PATH /Users/marek.skrobacki/bin/
 
 # other aliases
+alias cbast="ssh cbast1.lon3.corp.rackspace.com"
+
 
 
 # private stuff...
