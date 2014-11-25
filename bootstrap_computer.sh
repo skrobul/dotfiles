5bd29de6e8d032588e2248d8780edf336522415b
Marek Skrobacki <skrobul@skrobul.com>
2014-11-25 20:45:37 +0000

    convert brewfile into bootstrap script...

---
+++ bootstrap_computer.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+# Loosely based on https://github.com/thoughtbot/laptop/blob/master/mac
+
+fancy_echo() {
+  printf "\n%b\n" "$1"
+}
+
+append_to_zshrc() {
+  local text="$1" zshrc
+  local skip_new_line="$2"
+
+  if [[ -w "$HOME/.zshrc.local" ]]; then
+    zshrc="$HOME/.zshrc.local"
+  else
+    zshrc="$HOME/.zshrc"
+  fi
+
+  if ! grep -Fqs "$text" "$zshrc"; then
+    if (( skip_new_line )); then
+      printf "%s\n" "$text" >> "$zshrc"
+    else
+      printf "\n%s\n" "$text" >> "$zshrc"
+    fi
+  fi
+}
+
+trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
+
+set -e
+
+if [[ ! -d "$HOME/.bin/" ]]; then
+  mkdir "$HOME/.bin"
+fi
+
+if [[ ! -f "$HOME/.zshrc" ]]; then
+  touch "$HOME/.zshrc"
+fi
+
+append_to_zshrc 'export PATH="$HOME/.bin:$PATH"'
+
+if [[ "$SHELL" != */zsh ]]; then
+  fancy_echo "Changing your shell to zsh ..."
+    chsh -s $(which zsh)
+fi
+
+brew_install_or_upgrade() {
+  if brew_is_installed "$1"; then
+    if brew_is_upgradable "$1"; then
+      brew upgrade "$@"
+    fi
+  else
+    brew install "$@"
+  fi
+}
+
+brew_is_installed() {
+  local name="$(brew_expand_alias "$1")"
+
+  brew list -1 | grep -Fqx "$name"
+}
+
+brew_is_upgradable() {
+  local name="$(brew_expand_alias "$1")"
+
+  brew outdated --quiet "$name" >/dev/null
+  [[ $? -ne 0 ]]
+}
+
+brew_expand_alias() {
+  brew info "$1" 2>/dev/null | head -1 | awk '{gsub(/:/, ""); print $1}'
+}
+
+brew_launchctl_restart() {
+  local name="$(brew_expand_alias "$1")"
+  local domain="homebrew.mxcl.$name"
+  local plist="$domain.plist"
+
+  mkdir -p "$HOME/Library/LaunchAgents"
+  ln -sfv "/usr/local/opt/$name/$plist" "$HOME/Library/LaunchAgents"
+
+  if launchctl list | grep -q "$domain"; then
+    launchctl unload "$HOME/Library/LaunchAgents/$plist" >/dev/null
+  fi
+  launchctl load "$HOME/Library/LaunchAgents/$plist" >/dev/null
+}
+
+brew_cask_install() {
+    fancy_echo "Installing CASK: $1"
+    brew install $@
+}
+
+if ! command -v brew >/dev/null; then
+  fancy_echo "Installing Homebrew, a good OS X package manager ..."
+    ruby <(curl -fsS https://raw.githubusercontent.com/Homebrew/install/master/install)
+
+    append_to_zshrc '# recommended by brew doctor'
+    append_to_zshrc 'export PATH="/usr/local/bin:$PATH"' 1
+    export PATH="/usr/local/bin:$PATH"
+else
+  fancy_echo "Homebrew already installed. Skipping ..."
+fi
+
+fancy_echo "Updating Homebrew formulas ..."
+brew update
+
+
+
+if ! command -v rcup >/dev/null; then
+  fancy_echo "Installing rcm, to manage your dotfiles ..."
+    brew tap thoughtbot/formulae
+    brew_install_or_upgrade 'rcm'
+else
+  fancy_echo "rcm already installed. Skipping ..."
+fi
+
+# Config files
+env RCRC=$HOME/dotfiles/rcrc rcup -v
+
+# Cask
+brew tap caskroom/cask
+brew install brew-cask
+
+CASKS_TO_INSTALL="
+alfred
+caffeine
+flux
+virtualbox
+vagrant
+google-chrome
+iterm2
+sequel-pro
+dash
+adium
+lightpaper
+slack
+vlc
+the-unarchiver
+dropbox
+box-sync
+spotify
+evernote
+seil
+karabiner
+macfusion
+disk-inventory-x
+macfusion
+boot2docker
+calibre
+cord
+keepassx
+rescuetime
+skitch
+java
+skype
+"
+
+for cask in $CASKS_TO_INSTALL; do 
+    fancy_echo "Installing cask $cask"
+     brew cask install $cask
+done
+# Brews...
+
+# custom taps
+
+brew tap thoughtbot/formulae
+
+# brews
+brew install rcm
+brew install reattach-to-user-namespace
+brew install tmux
+brew install encfs
+brew install fleetctl
+brew install irssi
+brew install git
+brew install nmap
+brew install xz
+brew install brew-cask
+brew install node
+brew install postgresql
+brew install imagemagick
+
+# bugfixes
+brew install openssl
+brew link -f openssl
+
