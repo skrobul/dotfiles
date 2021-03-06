ab44b4f4ad4620f483c1c8c9d925a3d1867f4632
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2021-03-06 13:19:28 +0000

    offlineimap: migrate to oauth2 and secret-tool
    python2 support for dbus has become very problematic.

--- offlineimap.py
+++ offlineimap.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python2
-# don't forget to
-#     sudo dnf install python2-dbus offlineimap
-#     pip2 install keyring secretstorage
-# offlineimap does not use python3 yet 
-import keyring
-import secretstorage
-
+# don't forget to install secret-tool
+import subprocess
 def get_password(host, username):
-  backend = keyring.get_keyring()
-  collection = backend.get_preferred_collection()
-  for item in collection.search_items({"service": "smtp", "host": host, "user": username}):
-    return item.get_secret()# .decode('utf-8')
+  return subprocess.check_output([
+      "secret-tool",
+      "lookup",
+      "service",
+      "smtp",
+      "host",
+      host,
+      "user",
+      username])
+
+def get_custom(host, username, cred_type):
+  ret= subprocess.check_output([
+      "secret-tool",
+      "lookup",
+      "service",
+      "smtp",
+      "host",
+      host,
+      "user",
+      username,
+      "type",
+      cred_type]).splitlines()[0]
+  return ret
