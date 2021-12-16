cf08b5ffa0d58564489e2ebf321f16df501a357e
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2021-12-16 23:40:58 +0000

    vim: replace coc.nvim with native lspconfig

---
+++ install-language-servers.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+NPM_MODULES="emmet-ls bash-language-server dockerfile-language-server-nodejs vscode-langservers-extracted @tailwindcss/language-server typescript typescript-language-server vls yaml-language-server"
+
+
+yarn global add $NPM_MODULES
