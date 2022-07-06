f6154269e509258f154af0eb896bb2290735c5bb
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2022-07-06 14:28:53 +0100

    block motions on treesitter

--- config/nvim/lua/skrobul/plugins/treesitter.lua
+++ config/nvim/lua/skrobul/plugins/treesitter.lua
@@ -1,32 +1,34 @@
 require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
+    highlight = {
+        enable = true,
     },
-  },
-  indent = { enable =  true },
-  ensure_installed = "maintained",
-  textobjects = {
-    select = {
-      enable = true,
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+        },
+    },
+    indent = { enable =  true },
+    ensure_installed = "maintained",
+    textobjects = {
+        select = {
+            enable = true,
 
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
 
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+                ["ib"] = "@block.inner",
+                ["ob"] = "@block.outer",
             }
-          }
         }
-      }
+    }
+}
