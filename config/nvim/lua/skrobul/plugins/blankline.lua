bdedf6da3be1e73b84f2d2c51773f0b8169200b6
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-01-07 15:06:49 +0000

    nvim: blankline colors

--- config/nvim/lua/skrobul/plugins/blankline.lua
+++ config/nvim/lua/skrobul/plugins/blankline.lua
@@ -1,5 +1,7 @@
+vim.cmd [[hi IndentBlanklineChar guifg=#212230 gui=nocombine]]
+
 require("indent_blankline").setup {
     show_current_context = true,
     show_current_context_start = true,
-    enabled = false
+    enabled = false,
 }
