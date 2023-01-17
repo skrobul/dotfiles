5714240bc90c725267dad77babea32fc375957c0
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-01-17 09:13:25 +0000

    lualine: extra description

--- config/nvim/lua/skrobul/plugins/lualine.lua
+++ config/nvim/lua/skrobul/plugins/lualine.lua
@@ -17,6 +17,7 @@ require('lualine').setup({
                 path = 1,                -- 0: Just the filename
                 -- 1: Relative path
                 -- 2: Absolute path
+                -- 3: Absolute with tilde
 
                 shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
                 -- for other components. (terrible name, any suggestions?)
