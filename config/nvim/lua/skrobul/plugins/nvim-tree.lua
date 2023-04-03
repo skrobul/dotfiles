126f2339f7ede6594c0bcc77f79818f4e42ee7a9
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-04-03 14:47:27 +0200

    nvim: re-enable the netrw for remote editing

--- config/nvim/lua/skrobul/plugins/nvim-tree.lua
+++ config/nvim/lua/skrobul/plugins/nvim-tree.lua
@@ -1,16 +1,15 @@
 -- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- OR setup with some options
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
+  disable_netrw = false,
+  hijack_netrw = true,
   renderer = {
     group_empty = true,
     icons = {
