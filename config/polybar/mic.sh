d6a9ae9ef9d94f0c5daf121cb562718ba59d0103
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2023-11-22 14:41:47 +0000

    i3: add global push-to-talk

---
+++ config/polybar/mic.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sink="$1"
+sleep_pid=0
+
+trap "toggle" USR1
+
+toggle() {
+  if [ "$sleep_pid" -ne 0 ]; then
+    kill "$sleep_pid" >/dev/null 2>&1
+  fi
+}
+
+while true; do
+  result=$(pactl get-source-mute "$sink")
+
+  if [[ "$result" == "Mute: yes" ]]; then
+    echo "muted"
+  else
+    echo "%{F#ff0000}UNMUTED"
+  fi
+
+  sleep 2 & sleep_pid=$! wait
+done
