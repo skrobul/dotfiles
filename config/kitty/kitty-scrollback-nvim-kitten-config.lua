b2da3e9571ddae8132b24a67b85476199c7a69f3
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-12-09 13:19:00 +0000

    kitty fixes
    including window splits

--- config/kitty/kitty-scrollback-nvim-kitten-config.lua
+++ config/kitty/kitty-scrollback-nvim-kitten-config.lua
@@ -7,11 +7,10 @@ vim.g.maplocalleader = ','
 vim.keymap.set({ 'n' }, '<C-e>', '5<C-e>', {})
 vim.keymap.set({ 'n' }, '<C-y>', '5<C-y>', {})
 
-vim.keymap.set({ 'v' }, '<CR>', '<Plug>(KsbVisualYank)')
 
 -- add kitty-scrollback.nvim to the runtimepath to allow us to require the kitty-scrollback module
 -- pick a runtimepath that corresponds with your package manager, if you are not sure leave them all it will not cause any issues
 vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/lazy/kitty-scrollback.nvim') -- lazy.nvim
-require('kitty-scrollback').setup({
-  -- put your kitty-scrollback.nvim configurations here
-})
+require('kitty-scrollback').setup({ })
+
+vim.keymap.set({ 'v' }, '<CR>', '<Plug>(KsbVisualYank)')
