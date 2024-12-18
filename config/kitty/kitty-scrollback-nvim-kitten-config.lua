daee046cdd00dd4201ede8f9ca7c8885bc5b924f
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2024-12-18 12:09:44 +0000

    kitty scrollback buffer yank fixes

--- config/kitty/kitty-scrollback-nvim-kitten-config.lua
+++ config/kitty/kitty-scrollback-nvim-kitten-config.lua
@@ -11,6 +11,16 @@ vim.keymap.set({ 'n' }, '<C-y>', '5<C-y>', {})
 -- add kitty-scrollback.nvim to the runtimepath to allow us to require the kitty-scrollback module
 -- pick a runtimepath that corresponds with your package manager, if you are not sure leave them all it will not cause any issues
 vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/lazy/kitty-scrollback.nvim') -- lazy.nvim
-require('kitty-scrollback').setup({ })
+require('kitty-scrollback').setup({})
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  group = vim.api.nvim_create_augroup('KittyScrollbackNvimFileType', { clear = true }),
+  pattern = { 'kitty-scrollback' },
+  callback = function()
+    -- scroll to bottom
+    vim.print(vim.cmd.normal("G"))
+    return true
+  end,
+})
 
 vim.keymap.set({ 'v' }, '<CR>', '<Plug>(KsbVisualYank)')
+vim.keymap.set({ 'v' }, 'y', '<Plug>(KsbVisualYank)')
