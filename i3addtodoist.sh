527e97aec123202b72b982321bb280a258d02dac
Marek Skrobacki <marek.skrobacki@rackspace.co.uk>
2025-07-11 19:10:31 +0100

    i3: todoist

---
+++ i3addtodoist.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ROFI_THEME=/home/skrobul/dotfiles/config/rofi/launchers/type-4/style-6.rasi
+# Prompt the user for some input using rofi
+content=$(rofi -dmenu -p "New Todo: " -filter "#Inbox " -lines 0 -width 90 -theme "$ROFI_THEME")
+
+# Content will be empty if the user cancelled
+if [[ ! -z "$content" ]]; then
+    todoist quick "$content"
+fi
